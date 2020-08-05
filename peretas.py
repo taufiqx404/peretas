eefcc382d213be376dcc0a51085c8105873d1749
taufiqx404 <59577411+taufiqx404@users.noreply.github.com>
2020-08-06 04:40:09 +0800

    Update and rename README.md to peretas.py

---
+++ peretas.py
@@ -0,0 +1,127 @@
+import os
+import sys
+import time
+
+os.system( ' clear ' )
+time.sleep(1)
+os.system( ' figlet TAUFIQ X ' )
+print " WELCOME MY WORLD "
+time.sleep(1)
+print "┏━┳┳┳━┳┳┓
+       |━┫┃┃┏┫━┫┏┓
+       ┃┏┫┃┃┗┫┃┃┃┃
+       ┗┛┗━┻━┻┻┛┃┃
+       ┏┳┳━┳┳┳┓┏┫┣┳┓
+       ┃┃┃┃┃┃┃┃┣┻┫┃┃
+       ┣┓┃┃┃┃┣┫┃┏┻┻┫
+       ┗━┻━┻━┻ "
+print " ===================="
+print "            RED DEVIL                 "
+print "====================="
+print " AUTHOR : TAUFIQ X            "
+print " TEAM      : RED DEVIL OFF  "
+print " MEMBER : TAUFIQ X ,LUCKY,RISKY,BANDIT,PANGLIMA "
+print "============================================ "
+print 
+time.slepp(1)
+print " [+] PILIHAN TOOLS [+] "
+print " [1] CREATE VIRUS "
+print " [2] DDOS ATTACK "
+print " [3] SCRIPT DEFACE "
+print " [4] TROJAN "
+print " [5] BRUTE FORCE "
+print " [6[ RANSOMEWARE "
+print " [7] DARK FB "
+print " [8] SCAN IP WEB "
+print " [9] LACAK IP "
+print " [10] INSTALL BAHAN DULU GAN BIAR WORK"
+time.sleep(1)
+pilih = raw_input( ' [?] PILIH TOOLS : ' )
+if pilih =="1":
+        os.system('git clone https://github.com/saydog/Virus-droid-creator ')
+         time.sleep(1)
+         print " • PENGINSTALAN SELESAI •••••••••"
+         time.sleep(1)
+         print " • KETIK cd Virus-droid-creator "
+         time.sleep(1)
+         print " • KETIK python2 dog.py
+elif pilih =="2"
+          os.system('git clone https://github.com/cyweb/hammer')
+          time.sleep(1)
+          print " • PENGINSTALAN SELESAI ••••••••••"
+          time.sleep(1)
+          print " • KETIK cd hammer "
+          time.sleep(1)
+          print " • KETIK python2 hammer.py
+elif pilih =="3"
+           os.system('git clone https://github.com/MiSetya/NiceScript')
+           time.sleep(1)
+           print " • PENGINSTALAN SELESAI •••••••••••"
+           time.sleep(1)
+           print " • KETIK  cd NiceScript
+           time.sleep(1)
+           print " • KETIK  chmod 777 create.py
+           time.sleep(1)
+           print " • KETIK python2 create.py
+elif pilih =="4"
+           os.system('git clone https://github.com/Bl4ckDr460n/STrojans')
+           time.sleep(1)
+           print " • PENGINSTALAN SELESAI ••••••••••••"
+           time.sleep(1)
+           print " • KETIK cd STrojans
+           time.sleep(1)
+           print " • KETIK python2 STrojans.py
+elif pilih =="5"
+           os.system('git clone https://github.com/Senitopeng/fbbrute')
+           time.sleep(1)
+           print " • PENGINSTALAN SELESAI •••••••••••"
+           time.sleep(1)
+           print " KETIK cd fbbrute
+           time.sleep(1)
+           print " • KETIK python2 fbbrute.py
+elif pilih =="6"
+            os.system('https://github.com/JejakCyber/Ransomware')
+            time.sleep(1)
+            print " • PENGINSTALAN SELESAI •••••••••••"
+            time.sleep(1)
+            print " •KETIK cd Ransomware
+            time.sleep(1)
+            print " •KETIK python2 Ransomware.py
+elif pilih =="7"
+           os.system('git clone https://github.com/taufiqx404/red.py')
+           time.sleep(1)
+           print " • PENGINSTALAN SELESAI ••••••••••••"
+           time.sleep(1)
+           print " • KETIK red.py
+           time.sleep(1)
+           print " • KETIK ls
+           time.sleep(1)
+           print " • KETIK python2 devi-red
+elif pilih =="8"
+           os.system('git clone https://github.com/Bl4ckDr460n/ScanIp')
+           time.sleep(1)
+           print " • PENGINSTALAN SELESAI ••••••••••••••••"
+           time.sleep(1)
+           print " KETIK cd ScaIp
+           time.sleep(1)
+           print " KETIK python2 scanip.py
+elif pilih =="9"
+           os.system('git clone https://github.com/Bl4ckDr460n/IP-Location')
+           time.sleep(1)
+           print " PENGINSTALAN SELESAI •••••••••• "
+           time.sleep(1)
+           print " KETIK cd IP-Location "
+           print " KETIK python2 ipgeolocation.py -t (ip yang ingin anda lacak)
+elif pilih =="10"   
+           os.system('apt update && apt upgrade')
+           os.system('pkg install python')
+           os.system('pkg install python2')
+           os.system('pkg install nano')                            
+           os.system('pkg install git')
+           os.system('pip2 install requests mechanize')m
+           print " • PENGINSTALAN SELESAI GENKS •••••••••••• "
+           
+                       
+        
+         
+         
